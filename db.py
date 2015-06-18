86f03a9c893d4342b7822d773a5c053295df37ab
lindsim <lindsey@localhost.(none)>
2015-06-17 17:49:09 -0700

    Needs finishing touches and doc

---
+++ db.py
@@ -0,0 +1,195 @@
+import sqlite3
+
+#Connect to database
+conn = sqlite3.connect("office_hours.db") 
+#Turn foreign keys on
+conn.execute("PRAGMA foreign_keys = ON")
+#Create cursor object
+cursor = conn.cursor()
+
+'''Functions that interact with database'''
+
+def create():
+#Create the tables if not created already
+	cursor.execute("""CREATE TABLE IF NOT EXISTS tutors 
+					(name CHAR(20) PRIMARY KEY, 
+					start INTEGER, 
+					end INTEGER)""")
+
+	cursor.execute("""CREATE TABLE IF NOT EXISTS sessions 
+					(id INTEGER PRIMARY KEY, 
+					student_name CHAR(20), 
+					tutor_name CHAR(20), 
+					start INTEGER, 
+					end INTEGER, 
+					FOREIGN KEY(tutor_name) REFERENCES tutors(name))""")
+
+	return 
+
+#Add data to Tutors table
+def add_tutor(name, start, end):
+	if new_tutor(name):
+		cursor.execute("""INSERT INTO tutors (name, start, end)
+						VALUES (?, ?, ?)""", (name, int(start), int(end)))
+		conn.commit()
+		return "Added %s available %s to %s." % (name, convert(start), convert(end))
+	
+	else: 
+		return "ERROR: %s already in database. Tutor names must be unique." % (name)
+
+#Add data to Session table
+def add_session(stu_name, tu_name, start, end):
+	cursor.execute("""INSERT INTO sessions (student_name, tutor_name, start, end) 
+							VALUES (?, ?, ?, ?)""", (stu_name, tu_name, start, end))
+	conn.commit()
+	return "Scheduled %s with %s from %s to %s."%(stu_name, tu_name, convert(start), convert(end))
+
+#Retrieve tutor schedule
+def tutor_sched(name):
+	sched = cursor.execute("""SELECT student_name, start, end 
+								FROM sessions 
+								WHERE tutor_name= (?) 
+								ORDER BY start""", (name,))
+
+	return printable_format(sched)
+
+
+
+#retrieve student schedule
+def stu_sched(name):
+	sched = cursor.execute("""SELECT tutor_name, start, end 
+								FROM sessions 
+								WHERE student_name = (?) 
+								ORDER BY start""", (name,))
+	
+	return printable_format(sched)
+
+#disconnect from database
+def close_db():
+	conn.close()
+	return 
+
+''' Helper functions '''
+
+#Change time format for output
+def convert(time):
+	if int(time) == 0:
+		return "12:00am"
+
+	elif int(time) < 1200:
+		return time[:-2] + ":" + time[-2:] + "am"
+
+	elif int(time) < 1300: 
+		return time[:-2] + ":" + time[-2:] + "pm"
+	
+	else: 
+		new_time = str(int(time) - 1200)
+		return new_time[:-2] + ":" + new_time[-2:] + "pm"
+
+#nicely format schedule for printing
+def printable_format(sched):
+	sessions_list = ""
+	
+	for row in sched: 
+		sessions_list = sessions_list + "-- " + convert(row[1]) + " to " + convert(row[2]) + " with " + row[0] + "\n"
+	
+	print sessions_list
+	return
+
+#check for unique values
+def new_tutor(name):
+	cursor.execute("""SELECT name
+						FROM tutors 
+						WHERE name = ?""",(name,))
+	
+	results = cursor.fetchall()
+	
+	if len(results)== 0:
+		return True
+	else: 
+		return False
+
+#check if tutor works during those hours and if tutor is available
+def open_tu(name, start, end):
+#is tutor in database?
+	if new_tutor(name) == True: 
+		return "no_tutor"
+
+#find hours tutor works
+	hours = cursor.execute("""SELECT start, end
+						FROM tutors
+						WHERE name = ?""", (name,))
+
+	hours = cursor.fetchall()
+
+#make sure tutor works during session time
+	if hours[0][0] > int(start) or hours[0][1] < int(end):
+		return "unavail"
+
+#find tutor's session times
+	cursor.execute("""SELECT start, end 
+						FROM sessions 
+						WHERE tutor_name = ?
+						ORDER BY start""", (name,))
+
+	results = cursor.fetchall()
+
+	overlap_time = overlap(results, start, end)
+
+#check for overlap between tutor's sessions and requested session
+
+	if len(results) == 0:
+		return True
+
+	elif overlap_time == True: 
+		return "overlap"
+			
+	elif overlap_time == False:
+		return True
+
+	else:
+		return "ERROR"
+	
+
+#check if student already has session scheduled
+def open_stu(name, start, end):
+	cursor.execute("""SELECT start, end 
+						FROM sessions 
+						WHERE student_name = ?""", (name,))
+
+	results = cursor.fetchall()
+
+	if len(results) == 0 or overlap(results, start, end) == False:
+		return True
+
+	if overlap(results, start, end) == True:
+		return False
+
+#check for overlap with info from database and requested session start and end 
+def overlap(results, start, end):
+
+ 	start_i = int(start) 
+ 	end_i = int(end)
+
+ 	for x in range(len(results)):
+ 		#modify range so classes may start and end back to back
+		if start_i in xrange(results[x][0], results[x][1]):
+			return True
+		elif end_i in xrange(results[x][0], results[x][1]):
+			return True
+	return False
+
+
+
+'''Check database functions'''
+
+#Print tutors table
+def get_tutors():
+	for row in cursor.execute("""SELECT * FROM tutors"""):
+		print row
+
+#Print sessions table
+def get_sessions():
+	for row in cursor.execute("""SELECT * FROM sessions"""):
+		print row
+
---
+++ main.py
@@ -0,0 +1,138 @@
+import db
+
+
+''' Functions for commands'''
+
+def tutor (tu_name, start, end):
+
+	time_valid = check_time(start, end, 0)
+	name_valid = check_name(tu_name)
+	
+	if time_valid != True:
+		return time_valid
+
+	elif name_valid != True:
+		return name_valid
+
+	elif time_valid and name_valid:
+		return db.add_tutor(tu_name, start, end)
+	
+	else: 
+		return "ERROR: input error."
+
+
+def reserve (stu_name, tu_name, start, end):
+	
+	time_valid = check_time(start, end, 1)
+	name_valid = check_name(stu_name)
+	
+	if time_valid != True:
+		return time_valid
+	elif name_valid != True: 
+		return name_valid
+
+
+  
+	open_tutor = db.open_tu(tu_name, start, end)
+	
+	if open_tutor == "unavail":
+		return "ERROR: %s is not available during that time." % (tu_name)
+	
+	elif open_tutor == "overlap": 
+		return "ERROR: %s already has a student during that time." % (tu_name)
+	
+	elif open_tutor == "no_tutor":
+		return "ERROR: %s not in database." % (tu_name)
+
+	elif open_tutor == True:  
+		if db.open_stu(stu_name, start, end) == False:
+			return "ERROR: %s already has a tutor during that time." % (stu_name)
+	
+		else:
+			return db.add_session(stu_name, tu_name, start, end)
+
+	else:
+		return "ERROR: Unknown error."
+
+def schedule(tu_name):
+	return db.tutor_sched(tu_name)
+
+
+def student(stu_name):
+	return db.stu_sched(stu_name)
+
+
+'''Helper functions'''
+
+#Verify correct formats
+def check_name(name):
+	#make sure name is between 3 and 20 characters long, inclusive
+	if len(name) >= 3 and len(name) <= 20: #and not already in db?
+		return True 
+	
+	else:
+		return "ERROR: Name input is invalid."
+
+def check_time(time1_str, time2_str, num):
+	too_high = ["6", "7", "8", "9"]
+
+	#make sure minutes place < 6 to determine if military time is correct
+	if len(time1_str) >= 2 and time1_str[-2] in too_high: 
+			return "ERROR: Start time is invalid. Use military time."
+			
+	if len(time2_str) >= 2 and time2_str[-2] in too_high:
+			return "ERROR: End time is invalid. Use military time."
+
+	
+	#convert time to int to determine if in correct range
+	time1 = int(time1_str)
+	time2 = int(time2_str)
+
+
+	
+	if (time1 > 2359 or time1 < 0):
+		return "ERROR: Start time is invalid."
+	
+	if (time2 > 2359 or time2 < 0):
+		return "ERROR: End time is invalid."
+
+	if time1 > time2:
+		return "ERROR: End time cannot be before start time."
+
+	if (time1 > (time2 - 30)):
+			if num == 0:
+				return "ERROR: Tutors must be scheduled for at least half an hour."
+			else: 
+				return "ERROR: Tutoring sessions must be for at least half an hour."
+	else:
+		return True
+
+
+
+'''set up what to run when file is opened'''
+
+try:
+	while True:
+
+		input = raw_input('> ')
+		separated = input.split() #will remove extra spaces 
+		command = separated[0] 
+		arguments = separated[1:]
+		db.create()
+
+		if command == "tutor" and len(arguments) == 3:
+			print tutor(arguments[0], arguments[1], arguments[2])
+		elif command == "reserve" and len(arguments) == 4:
+			print reserve(arguments[0], arguments[1], arguments[2], arguments[3])
+		elif command == "schedule" and len(arguments) == 1:		
+			schedule(arguments[0])
+		elif command == "student" and len(arguments) == 1:
+			student(arguments[0])
+		elif command == "quit":
+			break
+		else: 
+			print "ERROR: Command or arguments invalid."
+
+except KeyboardInterrupt:
+	db.close_db()
+	pass
